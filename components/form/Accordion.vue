58eb08bdd2e2da2231cabf4ac1ef4c086ad5787e
Zaid Smeirat <zeid@mediaplus.com.jo>
2022-11-10 10:01:38 +0300

    test13

--- components/form/Accordion.vue
+++ components/form/Accordion.vue
@@ -77,9 +77,9 @@ const props = defineProps({
   date: String,
 });
 
-import closeIcon from "/static/icons/minus.svg";
+import closeIcon from "~/assets/icons/minus.svg";
 const close = ref(closeIcon);
-import plusIcon from "/static/icons/bplus.svg";
+import plusIcon from "~/assets/icons/bplus.svg";
 const plus = ref(plusIcon);
 
 const usersToggle = ref(false);
